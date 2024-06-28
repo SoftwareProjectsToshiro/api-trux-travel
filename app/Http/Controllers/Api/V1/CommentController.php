2e4679e40dddf7f2dc0c95600439e50e2e37c511
Kathia Medina <kmedinar1@upao.edu.pe>
2024-06-28 13:37:54 -0500

    handler error

--- app/Http/Controllers/Api/V1/CommentController.php
+++ app/Http/Controllers/Api/V1/CommentController.php
@@ -2,21 +2,29 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\CentralLogics\Helpers;
 use App\Http\Controllers\Controller;
 use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
 {
     public function store(Request $request)
     {
-        $validated = $request->validate([
+        $validated = Validator::make($request->all(),[
             'tour_package_id' => 'required|exists:tour_packages,id',
             'user_id' => 'required|exists:users,id',
             'content' => 'required|string',
             'title' => 'required|string',
             'rating' => 'required|integer',
         ]);
+
+        if ($validated->fails()) {
+            return response()->json([
+                'errors' => Helpers::error_processor($validated)
+            ], 403);
+        };
 
         Comment::create([
             'tour_package_id' => $validated['tour_package_id'],
